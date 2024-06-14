d296429fac791d72ff205277d1b792b4cf7a49a4
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-06-14 20:12:47 +0300

    intial commit: draft, nginx, dockerfile

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM python:3.12.4-slim-bookworm AS build-stage
+
+COPY requirements.txt ./
+
+RUN pip wheel --no-cache-dir --no-deps --wheel-dir /wheels -r requirements.txt
+
+
+FROM python:3.12.4-slim-bookworm AS serve-stage
+
+RUN apt-get update && apt-get install ffmpeg libsm6 libxext6 -y --no-install-recommends \
+    build-essential \
+    nginx \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+COPY . .
+
+COPY default.conf /etc/nginx/conf.d/default.conf
+
+COPY --from=build-stage /wheels .
+
+RUN pip install --no-cache ./wheels/*
+
+EXPOSE 8000
+
+CMD sh -c "uvicorn main:app --host 127.0.0.1 --port 8000 & nginx -g 'daemon off'"
---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI, Query
+from typing import Annotated
+
+import aiohttp
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {
+        "status": "alive",
+        "info": "call /docs to see the documentation"
+    }
+
+
+@app.get("/currency-rate", description="get the currency rate of 'convert_to' based on 'convert_from'")
+async def currency_rate_handler(
+    from_currency: Annotated[str | None, Query(description="your currency you want to convert from")] = None, 
+    to_currency: Annotated[str | None, Query(description="currency you want to get value of")] = None, 
+    amount: Annotated[int | None, Query(description="the amount of from_currency")] = None
+):
+    if from_currency == None or to_currency == None or amount == None:
+        return {
+            "success": False,
+            "error_msg": "you must provide both sides, from_currency and to_currency, as well as amount"
+        }
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://cash.rbc.ru/cash/json/converter_currency_rate/?currency_from={from_currency}&currency_to={to_currency}&source=cbrf&sum={amount}&date=") as currency_response:
+            json = await currency_response.json()
+            return {
+                "success": True,
+                "currency_rate": json
+            }
+            
+  
+    
