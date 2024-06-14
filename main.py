55831f3319b2bb4cf4c03cc2ddea0e02f54a1fc3
Iggy Kuperson <i.kupchinenko@gmail.com>
2024-06-14 22:08:08 +0300

    fix: correctly formatted json

--- main.py
+++ main.py
@@ -32,8 +32,9 @@ async def currency_rate_handler(
     
     async with aiohttp.ClientSession() as session:
         async with session.get(f"https://cash.rbc.ru/cash/json/converter_currency_rate/?currency_from={from_currency}&currency_to={to_currency}&source=cbrf&sum={amount}&date=") as currency_response:
-            json_str = await json.dumps(currency_response.json())
-            currency = CurrencyModel.model_validate_json(json_data=json_str)
+            raw_json_response = await currency_response.json()
+            json_str_response = json.dumps(raw_json_response)
+            currency = CurrencyModel.model_validate_json(json_data=json_str_response)
             if currency.status == 200:
                 return {
                     "success": True,
